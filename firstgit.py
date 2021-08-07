5e6c992c5cf07ec0b60c803febad6ca718d87dc8
y5cho <y5cho@ucsd.edu>
2021-08-07 10:28:16 -0700

    Update firstgit.py

--- firstgit.py
+++ firstgit.py
@@ -1,6 +1,11 @@
+from random import randrange
 absent = [3, 7]
+vape = [randrange(0,15)]
 
 for student in range(1,11):
     if student in absent:
         continue 
+    elif student in vape:
+        print("{}th student, follow me to the main office".format(student))
+        break
     print("{}th student is here".format(student))
