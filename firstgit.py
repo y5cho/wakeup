8e7d1cc430e5135ace6739577a858c99ef2ca5d4
y5cho <y5cho@ucsd.edu>
2021-08-09 12:21:55 -0700

    Update firstgit.py

--- firstgit.py
+++ firstgit.py
@@ -1,7 +1,8 @@
 from random import randrange
+import random 
 start = randrange(1,5)
 end = randrange(5,11)
-absent = []
+students = []
 
 '''
 for student in range(start, end):
@@ -13,23 +14,34 @@ class Student:
         self.status = status 
         self.mood = mood
         if self.status != "good":
-            absent.append(self.name)
-            print("{} is not coming to school today due to {}".format(self.name, status))
+            print("{} is not coming to school today for being {}".format(self.name, status))
         else:
             print("{} is here".format(self.name))
-
-student1 = Student("Kim", "tired", "illness")
 
 class Math_Student(Student):
     def __init__(self, name):
         Student.__init__(self, name, "wonderous", "good")
+        students.append(self)
 
 class Sports_Student(Student):
     def __init__(self, name):
         Student.__init__(self, name, "hyped", "excused")
+        students.append(self)
 
 soccer_player = Sports_Student("Messi")
 math_student = Math_Student("Tao")
+ms_2 = Math_Student("Euler")
+football_player = Sports_Student("Metcalf")
+
+vape = random.choice(students)
+for i in students:
+    if i == vape:
+        print("student {}, follow me to the main office".format(i.name))
+
+
+
+
+
 '''
 vape = [randrange(0,15)]
 for student in range(1,11):
