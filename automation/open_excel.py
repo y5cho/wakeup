425f5f392fe0e18d2867617a70e7e604267ea60d
y5cho <y5cho@ucsd.edu>
2021-08-21 17:24:22 -0700

    commit

--- automation/open_excel.py
+++ automation/open_excel.py
@@ -7,8 +7,13 @@ wb = load_workbook("Book1.xlsx")
 sheets = wb.sheetnames
 Sheet1 = wb[sheets[0]]
 Sheet2 = wb.create_sheet("Sheet2")
-Sheet1.cell(row = 2, column = 4).value = 5 
 
+
+
+
+for y in range(2,12):
+    Sheet1.cell(row = y, column = 2).value = randint(1,100)
+    Sheet1.cell(row = y, column = 3).value = randint(1,100)
 
 
 wb.save("Book1.xlsx")
