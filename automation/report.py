19cb9268af569a1deb4c63127dea6df7979af9e1
y5cho <y5cho@ucsd.edu>
2021-08-31 16:04:16 -0700

    Update report.py

--- automation/report.py
+++ automation/report.py
@@ -3,12 +3,20 @@ from openpyxl import load_workbook
 from random import *
 from openpyxl.workbook.workbook import Workbook
 from openpyxl.utils.cell import coordinate_from_string
-'''
+
 file = load_workbook("Book1.xlsx")
 sheets = file.sheetnames 
 Sheet1 = file[sheets[0]]
 col_B = Sheet1["B"]
+'''
+#for row in Sheet1.iter_rows(min_row = 1, max_row=11, min_col=1, max_col=3):
+#   print(row[0].value, row[1].value, row[2].value)
 
-for row in Sheet1.iter_rows(min_row = 1, max_row=11, min_col=1, max_col=3):
-    print(row[0].value, row[1].value, row[2].value)
+text = "i like to eat apple. Me too. Let's go get some apple"
+sent_list =[]
+for sent in text.split('.'):
+    if 'apple' in sent:
+        sent_list.append(sent)
+
+print(sent_list)
 
